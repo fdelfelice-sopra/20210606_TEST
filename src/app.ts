61e3db6f59417c959a7c42d1423324897750ff62
Filippo Del Felice <filippo.delfelice@soprasteria.com>
2023-06-12 00:54:37 +0200

    Removed useless main

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,14 @@
 import { test } from "node:test";
 import employees from "./classes/employees";
 
-function main() {
-	// TODO load employee from file
-	const employeeFileData: string = `Doe, John, 1982/10/08, john.doe@foobar.com
-	Ann, Mary, 1975/06/12, mary.ann@foobar.com
-	Doe, John2, 1982/10/08, john.doe@foobar.com
-	Ann, Mary2, 1975/11/11, mary.ann@foobar.com
-	Doe, John3, 1982/12/08, john.doe@foobar.com
-	Ann, Mary3, 1975/02/11, mary.ann@foobar.com`;
 
-	const employeesGroup = new employees(employeeFileData);
-	employeesGroup.sendBirthdayGreetings();
-}
+// TODO load employee from file
+const employeeFileData: string = `Doe, John, 1982/10/08, john.doe@foobar.com
+Ann, Mary, 1975/06/12, mary.ann@foobar.com
+Doe, John2, 1982/10/08, john.doe@foobar.com
+Ann, Mary2, 1975/11/11, mary.ann@foobar.com
+Doe, John3, 1982/12/08, john.doe@foobar.com
+Ann, Mary3, 1975/02/11, mary.ann@foobar.com`;
 
-main();
+const employeesGroup = new employees(employeeFileData);
+employeesGroup.sendBirthdayGreetings();
